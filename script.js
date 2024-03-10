11d4a734405b8f0b076b71b9228f2db919bce4cb
Shrutika Rajkuwar <rajkuwarshrutika@gmail.com>
2024-03-10 23:37:14 +0530

    Tycoon webpage created
    Tycoon webpage created

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var popup=document.getElementById("popup-1");
+
+function OpenPopup(){
+    // document.getElementById("popup-1").classList.toggle("active");
+    popup.classList.add("open-popup-box");
+}
+function ClosePopup(){
+    popup.classList.remove("open-popup-box");
+}
